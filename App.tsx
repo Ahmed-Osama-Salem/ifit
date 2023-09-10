5cfee7e51c0139723450be81e3f688b798e0748e
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-09-10 22:49:54 +0300

    ifit init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import LottieView from "lottie-react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.animationContainer}>
+        <LottieView
+          source={require("./assets/22.json")}
+          autoPlay
+          loop
+          style={{
+            width: 500,
+            height: "auto",
+          }}
+        />
+        <Text>Welcome to I-Fit</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  animationContainer: {
+    // backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
   },
 });
