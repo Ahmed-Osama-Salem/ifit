95bc4667e236abdc35902290a61fd7bbbcc88f18
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-09-11 21:13:46 +0300

    fix(lotti): fix autoplay for ios

--- App.tsx
+++ App.tsx
@@ -1,23 +1,46 @@
 import { StatusBar } from "expo-status-bar";
+import AnimatedLottieView from "lottie-react-native";
 import LottieView from "lottie-react-native";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { useEffect, useRef } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const ref = useRef<AnimatedLottieView>(null);
+
+  useEffect(() => {
+    setTimeout(() => ref.current?.play());
+
+    return () => {
+      ref.current?.reset();
+    };
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.animationContainer}>
         <LottieView
-          source={require("./assets/22.json")}
+          ref={ref}
           autoPlay
+          source={require("./assets/logo.json")}
           loop
           style={{
-            width: 500,
+            width: "100%",
             height: "auto",
           }}
+          autoSize
+          resizeMode="cover"
+          speed={0.9}
         />
-        <Text>Welcome to I-Fit</Text>
+        <Text
+          style={{
+            fontSize: 24,
+            color: "#231A16",
+            fontWeight: "500",
+          }}
+        >
+          IFit
+        </Text>
       </View>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -25,12 +48,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#F6E117",
     alignItems: "center",
     justifyContent: "center",
   },
   animationContainer: {
-    // backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
     flex: 1,
